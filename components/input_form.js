a13e83d088b98972390b5da1e0c6bbc55a1f05a1
satch09 <18578121+Satch09@users.noreply.github.com>
2022-10-11 21:18:12 +0200

    add report types for selection

---
+++ components/input_form.js
@@ -0,0 +1,48 @@
+import { useForm } from "react-hook-form";
+import useSwr from "swr";
+import styles from "../styles/Home.module.css";
+export default function InputForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data, error } = useSwr("/api/reports", fetcher);
+
+  if (error) return <div>Failed to load reports</div>;
+  if (!data) return <div>Loading...</div>;
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <ul className={styles.list}>
+        {data.map((input) => {
+          return (
+            <>
+              <li className={styles.list_item}>
+                <label key={input.register}>{input.label}</label>
+                <input
+                  key={input.label}
+                  {...input}
+                  {...register(input.register)}
+                  {...input.options}
+                />
+              </li>
+            </>
+          );
+        })}
+      </ul>
+      {/* register your input into the hook by invoking the "register" function */}
+
+      {/* include validation with required or other standard HTML validation rules */}
+      {/* <input {...register("exampleRequired", { required: true })} /> */}
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+}
---
+++ pages/api/reports.js
@@ -0,0 +1,49 @@
+const reports = [
+  {
+    label: "Project Name",
+    type: "text",
+    defaultValue: "Project Name",
+    register: "project_name",
+    options: { required: true },
+  },
+  {
+    label: "eB Number",
+    type: "text",
+    defaultValue: "eB Number",
+    register: "eb_number",
+    options: { required: true },
+  },
+  {
+    label: "Io List",
+    type: "checkbox",
+    defaultValue: null,
+    register: "report:io_list",
+  },
+  {
+    label: "Enclosure Schedule",
+    type: "checkbox",
+    defaultValue: null,
+    register: "report:enclosure_schedule",
+  },
+  {
+    label: "Cable Utilisation",
+    type: "checkbox",
+    defaultValue: null,
+    register: "report:cable_utilisation",
+  },
+  {
+    label: "Project Detail Report",
+    type: "checkbox",
+    defaultValue: null,
+    register: "report:project_detail_report",
+  },
+  {
+    label: "Termination Schedule",
+    type: "checkbox",
+    defaultValue: null,
+    register: "report:termination_schedule",
+  },
+];
+export default function handler(req, res) {
+  return res.status(200).json(reports);
+}
